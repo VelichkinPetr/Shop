73b57e75987d016cbfd743a7ebf2c59ab8426a03
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:44:46 +0300

    feat(database/repositories/profile): add repository for profile

---
+++ database/repositories/profile.py
@@ -0,0 +1,22 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from api.errors.Exceptions import SQLError
+from database.repositories import BaseRepo
+from models import Profile
+from schemas import ProfileRead
+
+
+class ProfileRepo(BaseRepo):
+
+    model = Profile
+
+    @classmethod
+    def get_by_user_id(cls, session: Session, user_id:int) -> ProfileRead | None:
+        try:
+            query = select(cls.model).where(cls.model.user_id == user_id)
+            profile = session.scalar(query)
+            return profile
+        except SQLAlchemyError as db_error:
+            raise SQLError('Error in',cls.model.__name__, db_error)
