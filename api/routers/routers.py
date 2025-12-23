5918f33614c8db6ed6a292602df2a9b1b74642e1
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:42:24 +0300

    feat(api/routers/routers): add router for all routers

---
+++ api/routers/routers.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from api.routers.v1 import *
+
+
+api_router = APIRouter()
+
+api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(user_router, prefix="/users", tags=["users"])
+api_router.include_router(product_router, prefix="/products", tags=["products"])
+api_router.include_router(category_router, prefix="/categories", tags=["categories"])
+api_router.include_router(cart_router, prefix="/cart", tags=["cart"])
+api_router.include_router(order_router, prefix="/orders", tags=["orders"])
