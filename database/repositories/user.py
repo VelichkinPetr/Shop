6ea93d003a530a507d622f48e21d598a6ff851ac
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:44:36 +0300

    feat(database/repositories/user): add repository for user

---
+++ database/repositories/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from api.errors.Exceptions import SQLError
+from database.repositories import BaseRepo
+
+from models import User
+
+
+class UserRepo(BaseRepo):
+
+    model = User
+
+    @classmethod
+    def get_by_email(cls, session: Session, email:str) -> User | None:
+        try:
+            query = select(cls.model).where(cls.model.email == email)
+            user = session.scalar(query)
+            return user
+        except SQLAlchemyError as db_error:
+            raise SQLError('Error in',cls.model.__name__, db_error)
