d967c650b1c5965423bf80cdbb5eb51659f6766a
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:41:27 +0300

    feat(api/routers/v1/product): add router for product

---
+++ api/routers/v1/product.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from api.errors.Exceptions import HTTPError
+from database import get_db
+from core.security import get_current_user, get_current_admin
+from services.product import ProductService
+
+
+from schemas import ProductRead, ProductUpdate, ProductCreate
+from schemas import UserPublic
+
+
+router = APIRouter()
+
+@router.post('',
+             response_model=ProductRead,
+             status_code=status.HTTP_201_CREATED)
+def create_product(
+            product_data: ProductCreate,
+            db: Session = Depends(get_db),
+            admin: UserPublic = Depends(get_current_admin)
+) -> ProductRead:
+    new_product = ProductService.create_product(db, product_data)
+    return new_product
+
+@router.get('',
+            response_model=list[ProductRead],
+            status_code=status.HTTP_200_OK)
+def list_products(
+            category_id: str = None,
+            min_price: int = None,
+            max_price: int = None,
+            limit: int = 20,
+            offset: int = 0,
+            db: Session = Depends(get_db),
+            user: UserPublic = Depends(get_current_user)
+) -> list[ProductRead]:
+    products = ProductService.get_by_filter(db, category_id, min_price, max_price, limit, offset)
+    return products
+
+@router.get('/{product_id}',
+            response_model=ProductRead,
+            status_code=status.HTTP_200_OK)
+def get_product(
+            product_id: int,
+            db: Session = Depends(get_db),
+            user: UserPublic = Depends(get_current_user)
+) -> ProductRead:
+    product = ProductService.get_product(db, product_id)
+    if product is None:
+        raise HTTPError.not_found
+    return product
+
+@router.put('/{product_id}',
+            response_model=ProductRead,
+            status_code=status.HTTP_202_ACCEPTED)
+def update_product(
+            product_id: int,
+            product_data: ProductUpdate,
+            category_id: int = None,
+            db: Session = Depends(get_db),
+            admin: UserPublic = Depends(get_current_admin)
+) -> ProductRead:
+    if category_id is not None:
+        ProductService.add_category(db, product_id, category_id)
+    updated_product = ProductService.update_product(db, product_id, product_data)
+    if updated_product is None:
+        raise HTTPError.not_found
+    return updated_product
+
+
+@router.delete('/{product_id}',
+               status_code=status.HTTP_202_ACCEPTED)
+def delete_product(
+            product_id: int,
+            db: Session = Depends(get_db),
+            admin: UserPublic = Depends(get_current_admin)
+):
+    is_delete = ProductService.delete_product(db, product_id)
+    if not is_delete:
+        raise HTTPError.not_found
