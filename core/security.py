36446aaf311ecc1763c0c8ddc7fefc20ad0b51f0
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:43:09 +0300

    feat(core/security): add check auth for users and admins

---
+++ core/security.py
@@ -0,0 +1,31 @@
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.params import Depends
+from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+
+from database.session import get_db
+from services.user import UserService
+from api.errors.Exceptions import HTTPError
+from schemas import UserPublic
+
+
+basic_auth = HTTPBasic()
+password_hash = PasswordHash.recommended()
+
+def get_current_user(
+        db: Session = Depends(get_db),
+        credentials: HTTPBasicCredentials = Depends(basic_auth)
+) -> UserPublic:
+    user = UserService.get_me(db, email=credentials.username)
+    if user is None or not password_hash.verify(credentials.password, user.password.get_secret_value()):
+        raise HTTPError.unauthorized
+    return UserPublic(**user.model_dump(exclude={'password'}))
+
+def get_current_admin(
+        db: Session = Depends(get_db),
+        credentials: HTTPBasicCredentials = Depends(basic_auth)
+) -> UserPublic:
+    user = get_current_user(db, credentials=credentials)
+    if not user.is_admin:
+        raise HTTPError.unavailable
+    return UserPublic(**user.model_dump(exclude={'password'}))
