9ef30b7eea95e3d5daaefd2aef31e673e3bfe08e
Petr Velichkin <pyo7578@yandex.ru>
2025-12-23 23:39:43 +0300

    feat(api/errors): add custom errors

---
+++ api/errors/Exceptions.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException, status
+
+
+class SQLError(Exception):
+    pass
+
+class HTTPError(HTTPException):
+    exist = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="User already exists")
+
+    not_found = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Not found")
+
+    unauthorized = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect email or password")
+
+    unavailable = HTTPException(
+        status_code=status.HTTP_406_NOT_ACCEPTABLE,
+        detail="function is unavailable")
+
+    db_error = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Database error"
+    )
