65b7bfdb95eae45094a392c12dd10256b7b52694
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:35:22 +0300

    feat(services/init): create import names for services

---
+++ services/__init__.py
@@ -0,0 +1,15 @@
+from .user import UserService
+from .product import ProductService
+from .order import OrderService
+from .category import CategoryService
+from .cart import CartService
+from .auth import AuthService
+
+__all__ = [
+    'UserService',
+    'ProductService',
+    'OrderService',
+    'CategoryService',
+    'CartService',
+    'AuthService',
+]
