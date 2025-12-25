7fdb1c9f9ce5f482e6a6699f8caa11afb79015ab
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:37:25 +0300

    feat(services/auth): redesigned registration -> when user is created, empty profile and  shopping cart are created.

--- services/auth.py
+++ services/auth.py
@@ -1,24 +1,28 @@
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from api.errors.Exceptions import HTTPError
+from api.errors.Exceptions import HTTPError, SQLError
 from core.security import password_hash
-from database.repositories import UserRepo
-from models import User
-from schemas import UserCreate
+from schemas import UserCreate, UserPublic, CartCreate
+from services.cart import CartService
+from services.user import UserService
 
 
 class AuthService:
-    user_model: User
+    user_service = UserService
+    cart_service = CartService
 
     @classmethod
-    def registration(cls, session: Session, user_data: UserCreate):
-        # 1. Создать хэш пароля
+    def registration(cls,
+                     db: Session,
+                     user_data: UserCreate
+    ) -> UserPublic:
         hashed_password = password_hash.hash(user_data.password.get_secret_value())
-        # 2. Сохранить данные в БД
         user_data.password = hashed_password
         try:
-            UserRepo().create(session, user_data)
-        except IntegrityError:
+            new_user = cls.user_service.create_user(db, user_data)
+            empty_profile = cls.user_service.create_profile(db, new_user.id)
+            cart_data = CartCreate(profile_id=empty_profile.id)
+            new_cart = cls.cart_service.create_cart(db, cart_data)
+            return UserPublic(**new_user.model_dump(exclude={'password'}))
+        except SQLError:
             raise HTTPError.exist
-        return None
