38e40fa0f88f20228c577432da9c11d22ffa4fd1
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:28:48 +0300

    refactor(database/repositories/profile): lite refactor structure file

--- database/repositories/profile.py
+++ database/repositories/profile.py
@@ -13,10 +13,13 @@ class ProfileRepo(BaseRepo):
     model = Profile
 
     @classmethod
-    def get_by_user_id(cls, session: Session, user_id:int) -> ProfileRead | None:
+    def get_by_user_id(cls,
+                       db: Session,
+                       user_id:int
+    ) -> ProfileRead | None:
         try:
             query = select(cls.model).where(cls.model.user_id == user_id)
-            profile = session.scalar(query)
+            profile = db.scalar(query)
             return profile
         except SQLAlchemyError as db_error:
             raise SQLError('Error in',cls.model.__name__, db_error)
