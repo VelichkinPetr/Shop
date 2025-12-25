30a75a65c09771d83448aaaedd94a4ee22147c65
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:30:27 +0300

    feat(schemas/cart_item): add CartItemUpdate

--- schemas/cart_item.py
+++ schemas/cart_item.py
@@ -1,16 +1,20 @@
 from typing import Optional
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict
 
+from pydantic import BaseModel, ConfigDict
 
 
 class CartItemCreate(BaseModel):
     count: int
     product_id: int
 
+class CartItemUpdate(BaseModel):
+    count: Optional[int] = None
+
 class CartItem(CartItemCreate):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    cart_id: int
     created_at: datetime
     updated_at: datetime
