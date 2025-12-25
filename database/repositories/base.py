55e57d7e09329829e93bced2f643d6f9d0a54cba
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:27:13 +0300

    feat(database/repositories/base): add rollbacks and lite refactor structure file

--- database/repositories/base.py
+++ database/repositories/base.py
@@ -1,6 +1,6 @@
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 from pydantic import BaseModel
 
@@ -16,7 +16,10 @@ class BaseRepo:
     model = None
 
     @classmethod
-    def create(cls, db: Session, data: BaseModel) -> Base:
+    def create(cls,
+               db: Session,
+               data: BaseModel
+    ) -> Base:
         try:
             model = cls.model(**data.model_dump())
             db.add(model)
@@ -24,44 +27,58 @@ class BaseRepo:
             db.refresh(model)
             return model
         except SQLAlchemyError as db_error:
+            db.rollback()
             raise SQLError('Error in',cls.model.__name__, db_error)
 
-
     @classmethod
-    def get_all(cls, session: Session) -> list[Base]:
+    def get_all(cls,
+                db: Session
+    ) -> list[Base]:
         query = select(cls.model)
-        elements = session.scalars(query).all()
+        elements = db.scalars(query).all()
         return elements
     
     @classmethod
-    def get_by_id(cls, session: Session, current_id:int) -> Base | None:
+    def get_by_id(cls,
+                  db: Session,
+                  current_id:int
+    ) -> Base | None:
         query = select(cls.model).where(cls.model.id == current_id)
-        element = session.scalar(query)
+        element = db.scalar(query)
         return element
 
     @classmethod
-    def delete_by_id(cls, session: Session, current_id: int) -> None:
+    def delete_by_id(cls,
+                     db: Session,
+                     current_id: int
+    ) -> None:
         try:
             query = select(cls.model).where(cls.model.id == current_id)
-            elem = session.scalar(query)
-            session.delete(elem)
-            session.commit()
+            elem = db.scalar(query)
+            db.delete(elem)
+            db.commit()
         except SQLAlchemyError as db_error:
+            db.rollback()
             raise SQLError('Error in',cls.model.__name__, db_error)
 
     @classmethod
-    def update(cls, session: Session, current_id: int, data: BaseModel) -> Base | None:
+    def update(cls,
+               db: Session,
+               current_id: int,
+               data: BaseModel
+    ) -> Base | None:
         try:
             query = select(cls.model).where(cls.model.id == current_id)
-            current_elem = session.scalar(query)
+            current_elem = db.scalar(query)
             
             for key, value in data.model_dump().items():
                 if value is not None:
                     setattr(current_elem, key, value)
 
-            session.commit()
-            session.refresh(current_elem)
+            db.commit()
+            db.refresh(current_elem)
             return current_elem
         except SQLAlchemyError as db_error:
+            db.rollback()
             raise SQLError('Error in',cls.model.__name__, db_error)
 
