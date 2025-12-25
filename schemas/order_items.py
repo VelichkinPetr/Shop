a469f5cf29cb46d6df48840e4c5413e5bf614327
Petr Velichkin <pyo7578@yandex.ru>
2025-12-25 15:34:11 +0300

    feat(schemas/order_items): rebase attributes: price, cost, order_id

--- schemas/order_items.py
+++ schemas/order_items.py
@@ -4,12 +4,11 @@ from pydantic import BaseModel, ConfigDict
 class OrderItemCreate(BaseModel):
     quantity: int
     product_id: int
-
+    price: int
+    cost: int
+    order_id: int
 
 class OrderItem(OrderItemCreate):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    price: int
-    cost: int
-    order_id: int
